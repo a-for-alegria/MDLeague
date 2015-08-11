a1210900c23ac855c51e68e1b233f5133fdc26b2
a-for-alegria <alegria2112@gmail.com>
2015-08-11 12:29:39 +0300

    initial commit with basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mdleague::Application.config.secret_key_base = 'a84a607876727e02727d86443a4669f377b1c8fdfe306f250fabb7f8ad49d60de1e73a30b5760dc9985c73b5db6727372e3ab4f831c2424bc3ac6ee004bf2c88'
